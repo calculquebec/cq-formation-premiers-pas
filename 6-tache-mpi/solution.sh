0fefcdbf64b1cfdfc2bc4a2dac131b754e0d0b27
Pier-Luc St-Onge <pier-luc.st-onge@calculquebec.ca>
2016-09-27 19:44:42 -0400

    Exercice 6 pour Guillimin

--- 6-tache-mpi/solution.sh
+++ 6-tache-mpi/solution.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-#PBS -A colosse-users
-#PBS -l nodes=4:ppn=8
-#PBS -l walltime=16:00
+#PBS -A class
+#####
+# TODO: Request 2 full nodes of 12 processors
+#####
+#PBS -l nodes=2:ppn=12
+#PBS -l walltime=10:00
+#PBS -N ex6
 
-module load libs/boost libs/image_magick
+module load iomkl/2015b Boost/1.59.0-Python-2.7.10 ImageMagick/7.0.1-6
 cd ${PBS_O_WORKDIR}
 
-mpiexec ../filterImage.exe --filters monochrome --files ../photos/*
-
-
+#####
+# TODO: Use mpiexec and filterImage-mpi.exe
+#####
+mpiexec ../filterImage-mpi.exe --filters grayscale add_noise monochrome --files ../../pictures/*
--- 6-tache-mpi/submit.sh
+++ 6-tache-mpi/submit.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-#PBS -A colosse-users
-##### 
-# TODO: Changer le nombre de noeuds utilisés pour en utiliser 4
+#PBS -A class
 #####
-#PBS -l nodes=1:ppn=8
-#PBS -l walltime=16:00
+# TODO: Request 2 full nodes of 12 processors
+#####
+#PBS -l nodes=1:ppn=4
+#PBS -l walltime=10:00
+#PBS -N ex6
 
-module load libs/boost libs/image_magick
+module load iomkl/2015b Boost/1.59.0-Python-2.7.10 ImageMagick/7.0.1-6
 cd ${PBS_O_WORKDIR}
 
-####
-# TODO: Appeler l'exécutable ../filterImage.exe avec mpiexec
-####
- ../filterImage.exe --filters monochrome --files ../photos/*
-
-
+#####
+# TODO: Use mpiexec and filterImage-mpi.exe
+#####
+ ../filterImage.exe --filters grayscale add_noise monochrome --files ../../pictures/*
