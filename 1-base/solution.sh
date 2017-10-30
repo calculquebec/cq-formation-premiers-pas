adb7eea963d781f41ad8fa40473d5d48b03ac5ea
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2017-10-30 11:03:46 -0400

    removed reservation flag

--- 1-base/solution.sh
+++ 1-base/solution.sh
@@ -3,6 +3,5 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=2:00
 #SBATCH --account=def-mboisson 		      # Use your own account here
-#SBATCH --reservation=training 		      # This is only needed for this training. Remove it after.
 echo "bonjour"
 
