a680741db63452f41db27bd746f7ac3269114ffb
Maxime Boissonneault <maxime.boissonneault@calculquebec.ca>
2017-10-30 11:03:24 -0400

    added reservation flag

--- 1-base/solution.sh
+++ 1-base/solution.sh
@@ -3,4 +3,6 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=2:00
 #SBATCH --account=def-mboisson 		      # Use your own account here
+#SBATCH --reservation=training 		      # This is only needed for this training. Remove it after.
 echo "bonjour"
+
