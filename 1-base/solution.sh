7800125633adcf8042c7385c83e98f5057b5df05
Pier-Luc St-Onge <pier-luc.st-onge@calculquebec.ca>
2019-06-12 18:32:11 -0400

    Updated 1-base with README.en

--- 1-base/solution.sh
+++ 1-base/solution.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+#SBATCH --account=def-sponsor00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --time=2:00
-#SBATCH --account=def-mboisson 		      # Use your own account here
-echo "bonjour"
+#SBATCH --job-name=ex1
 
+echo "Bonjour"
--- 1-base/submit.sh
+++ 1-base/submit.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-echo "bonjour"
+
+echo "Bonjour"
