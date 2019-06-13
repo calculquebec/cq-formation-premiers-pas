8b7b69abef36f5db43b4cf2b19ffed953db43a32
Pier-Luc St-Onge <pier-luc.st-onge@calculquebec.ca>
2019-06-13 17:14:29 -0400

    Updated 3-gnu-parallel with README.en

--- 3-gnu-parallel/solution.sh
+++ 3-gnu-parallel/solution.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
+#SBATCH --account=def-sponsor00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
-#SBATCH --mem-per-cpu=1G
+#SBATCH --mem-per-cpu=1000M
 #SBATCH --time=5:00
-#SBATCH --account=def-mboisson 		      # Use your own account here
+#SBATCH --job-name=ex3
 
 module load gcc boost
-SRCDIR=/project/6002799/photos/
 
-parallel ../filterImage.exe --filters grayscale --srcdir $SRCDIR --files {1} ::: $(ls $SRCDIR)
+SRCDIR=/project/def-sponsor00/photos/
 
-
+parallel ../filterImage.exe --srcdir $SRCDIR --files {1} --filters grayscale ::: $(ls $SRCDIR)
--- 3-gnu-parallel/submit.sh
+++ 3-gnu-parallel/submit.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
+#SBATCH --account=def-sponsor00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #####
 # TODO: Ajouter l'option --cpus-per-task
 #####
-#SBATCH --mem-per-cpu=1G
+#SBATCH --mem-per-cpu=1000M
 #SBATCH --time=5:00
-#SBATCH --account=def-mboisson 		      # Use your own account here
+#SBATCH --job-name=ex3
 
-module load boost
-SRCDIR=/project/6002799/photos/
+module load gcc boost
+
+SRCDIR=/project/def-sponsor00/photos/
 
 #####
 # TODO: Ajouter la directive parallel avec les bons fichiers et arguments
 #####
-../filterImage.exe --filters grayscale --srcdir $SRCDIR --files <fichier.jpg>
-
-
+../filterImage.exe --srcdir $SRCDIR --files ... --filters grayscale
