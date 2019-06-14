bbd2a0f3083eba2c971376d3655ebf4b988e94eb
Pier-Luc St-Onge <pier-luc.st-onge@calculquebec.ca>
2019-06-14 14:29:17 -0400

    Updated 4-lot-de-taches with README.en

--- 4-lot-de-taches/solution.sh
+++ 4-lot-de-taches/solution.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
+#SBATCH --account=def-sponsor00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
-#SBATCH --mem-per-cpu=1G
+#SBATCH --mem-per-cpu=1000M
 #SBATCH --time=5:00
-#SBATCH --account=def-mboisson 		      # Use your own account here
 #SBATCH --array=0-8:1
+#SBATCH --job-name=ex4
 
-module load boost
-SRCDIR=/project/6002799/photos/
+module load gcc boost
+
+SRCDIR=/project/def-sponsor00/photos/
 
 # Liste des filtres disponibles
 FILTERS=(grayscale edges emboss negate solarize flip flop monochrome add_noise)
 
-#####
-# TODO: Ajouter la directive parallel avec les bons fichiers et arguments
-#####
-parallel ../filterImage.exe --srcdir $SRCDIR --filters ${FILTERS[$SLURM_ARRAY_TASK_ID]} --files {1} ::: $(ls $SRCDIR)
-
-
+parallel ../filterImage.exe --srcdir $SRCDIR --files {1} --filters ${FILTERS[$SLURM_ARRAY_TASK_ID]} ::: $(ls $SRCDIR)
--- 4-lot-de-taches/submit.sh
+++ 4-lot-de-taches/submit.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
+#SBATCH --account=def-sponsor00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=2
-#SBATCH --mem-per-cpu=1G
+#SBATCH --mem-per-cpu=1000M
 #SBATCH --time=5:00
-#SBATCH --account=def-mboisson 		      # Use your own account here
 #####
 # TODO: Ajouter la bonne option pour le lot de tâches
 #####
+#SBATCH --job-name=ex4
 
 module load gcc boost
-SRCDIR=/project/6002799/photos/
+
+SRCDIR=/project/def-sponsor00/photos/
 
 # Liste des filtres disponibles
 FILTERS=(grayscale edges emboss negate solarize flip flop monochrome add_noise)
@@ -19,6 +21,4 @@ FILTERS=(grayscale edges emboss negate solarize flip flop monochrome add_noise)
 # TODO: Utiliser le tableau FILTERS pour sélectionner le bon filtre en fonction de la valeur
 # de SLURM_ARRAY_TASK_ID
 #####
-parallel ../filterImage.exe --srcdir $SRCDIR --filters FIXME --files {1} ::: $(ls $SRCDIR)
-
-
+parallel ../filterImage.exe --srcdir $SRCDIR --files {1} --filters FIXME ::: $(ls $SRCDIR)
