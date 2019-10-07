8cb35fb91c8c4c2c9e7cd552769b5d7b3d47b93f
Pier-Luc St-Onge <pier-luc.st-onge@calculquebec.ca>
2019-10-07 16:54:27 -0400

    Simplified exercise 2, translated comments

--- 2-sequentielles/solution.sh
+++ 2-sequentielles/solution.sh
@@ -7,8 +7,21 @@
 #SBATCH --time=5:00
 #SBATCH --job-name=ex2
 
+# EN: Load all required modules
+# FR: Charger les modules nécessaires
 module load gcc boost
 
+# EN: If needed, adjust the following path
+# FR: Ajuster, au besoin, le chemin suivant
 SRCDIR=/project/def-sponsor00/photos/
+
+####
+# EN: Add required arguments to the ../filterImage.exe command in order
+#     to transform one picture into grayscale. To list available arguments:
+#     ../filterImage.exe --help
+# FR: Ajouter les bons arguments à la commande ../filterImage.exe
+#     pour transformer une photo en grayscale. Pour trouver les bon arguments:
+#     ../filterImage.exe --help
+####
 
 ../filterImage.exe --srcdir $SRCDIR --files $(ls $SRCDIR|head -1) --filters grayscale
--- 2-sequentielles/submit.sh
+++ 2-sequentielles/submit.sh
@@ -7,16 +7,21 @@
 #SBATCH --time=5:00
 #SBATCH --job-name=ex2
 
-# TODO: Charger les modules nécessaires
+# EN: Load all required modules
+# FR: Charger les modules nécessaires
 module load ....
 
-# TODO: Ajuster, au besoin, le chemin suivant
+# EN: If needed, adjust the following path
+# FR: Ajuster, au besoin, le chemin suivant
 SRCDIR=/project/def-sponsor00/photos/
 
 ####
-# TODO: Ajouter les bons arguments à la commande ../filterImage.exe
-#       pour transformer une première photo en grayscale.
-# Utilisez ../filterImage.exe --help afin de trouver les arguments à utiliser
+# EN: Add required arguments to the ../filterImage.exe command in order
+#     to transform one picture into grayscale. To list available arguments:
+#     ../filterImage.exe --help
+# FR: Ajouter les bons arguments à la commande ../filterImage.exe
+#     pour transformer une photo en grayscale. Pour trouver les bon arguments:
+#     ../filterImage.exe --help
 ####
 
-../filterImage.exe --srcdir $SRCDIR ......
+../filterImage.exe --srcdir $SRCDIR --files $(ls $SRCDIR|head -1) ...
