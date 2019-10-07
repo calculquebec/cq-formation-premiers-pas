abce118b4d0f1af8a5cbc1cb360e68f2b2254718
Pier-Luc St-Onge <pier-luc.st-onge@calculquebec.ca>
2019-10-07 17:09:00 -0400

    Translated comments in exercise 5

--- 5-tache-mpi/submit.sh
+++ 5-tache-mpi/submit.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #SBATCH --account=def-sponsor00
 ##### 
-# TODO: Changer la configuration pour utiliser 2 noeuds, 2 tâches par noeud et 1 CPU par tâche
+# EN: Change the configuration to request 2 nodes, 2 tasks per node and 1 CPU per task
+# FR: Changer la configuration pour utiliser 2 noeuds, 2 tâches par noeud et 1 CPU par tâche
 #####
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
@@ -16,6 +17,7 @@ SRCDIR=/project/def-sponsor00/photos/
 FILTERS="grayscale edges emboss negate solarize flip flop monochrome add_noise"
 
 ####
-# TODO: Appeler l'exécutable ../filterImage.exe avec mpiexec
+# EN: Call the executable ../filterImage.exe with mpiexec
+# FR: Appeler l'exécutable ../filterImage.exe avec mpiexec
 ####
 ../filterImage.exe --srcdir $SRCDIR --files $(ls $SRCDIR) --filters $FILTERS
