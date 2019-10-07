21108ac4f661ddc1b0147baf515a8e6e304882c0
Pier-Luc St-Onge <pier-luc.st-onge@calculquebec.ca>
2019-10-07 17:05:50 -0400

    Translated comments in exercise 4

--- 4-lot-de-taches/submit.sh
+++ 4-lot-de-taches/submit.sh
@@ -6,7 +6,8 @@
 #SBATCH --mem-per-cpu=1000M
 #SBATCH --time=5:00
 #####
-# TODO: Ajouter la bonne option pour le lot de tâches
+# EN: Add the proper option for the array of jobs
+# FR: Ajouter la bonne option pour le lot de tâches
 #####
 #SBATCH --job-name=ex4
 
@@ -18,7 +19,9 @@ SRCDIR=/project/def-sponsor00/photos/
 FILTERS=(grayscale edges emboss negate solarize flip flop monochrome add_noise)
 
 #####
-# TODO: Utiliser le tableau FILTERS pour sélectionner le bon filtre en fonction de la valeur
-# de SLURM_ARRAY_TASK_ID
+# EN: Use the array FILTERS to select a filter according to the value of
+#     SLURM_ARRAY_TASK_ID
+# FR: Utiliser le tableau FILTERS pour sélectionner le bon filtre en fonction de
+#     la valeur de SLURM_ARRAY_TASK_ID
 #####
 parallel ../filterImage.exe --srcdir $SRCDIR --files {1} --filters FIXME ::: $(ls $SRCDIR)
