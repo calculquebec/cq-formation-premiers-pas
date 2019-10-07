fc233b98df63f91cf48ab43c0fa2783c7f92dd60
Pier-Luc St-Onge <pier-luc.st-onge@calculquebec.ca>
2019-10-07 17:01:35 -0400

    Translated comments

--- 3-gnu-parallel/submit.sh
+++ 3-gnu-parallel/submit.sh
@@ -3,7 +3,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #####
-# TODO: Ajouter l'option --cpus-per-task
+# EN: Add the --cpus-per-task option
+# FR: Ajouter l'option --cpus-per-task
 #####
 #SBATCH --mem-per-cpu=1000M
 #SBATCH --time=5:00
@@ -14,6 +15,7 @@ module load gcc boost
 SRCDIR=/project/def-sponsor00/photos/
 
 #####
-# TODO: Ajouter la directive parallel avec les bons fichiers et arguments
+# EN: Add the "parallel" directive with proper arguments and a list of files
+# FR: Ajouter la directive parallel avec les bons fichiers et arguments
 #####
 ../filterImage.exe --srcdir $SRCDIR --files ... --filters grayscale
